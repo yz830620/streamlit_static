8129978ca422d2bdc8a31ee7d9184703196440cc
yz830620 <yz830620@gmail.com>
2020-12-21 15:15:08 +0800

    update

--- first.py
+++ first.py
@@ -1,16 +1,18 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
-st.title('My first app')
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
-st.write(
-    """
-# Apps with widgets!
 """
-)
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
+
+df
 x = st.slider("Select a number", 0, 100)
 st.write("You selected", x)
