adf021d0c9dad5b7c9fb8bad5373781f2569a429
yz830620 <yz830620@gmail.com>
2020-12-24 00:04:02 +0800

    update

--- first.py
+++ first.py
@@ -1,18 +1,55 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+import time
 """
-# My first app
-Here's our first attempt at using data to create a table:
+# 小兔觀察指南
+Here's our 兔兔情緒起伏 and 成長軌跡去了哪裡:
 """
 
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
+chart_data = pd.DataFrame(np.random.randn(20, 4), columns=["世紀帝國分數", "肚餓程度", "心情", "抱抱指數"])
 st.line_chart(chart_data)
 
-df
-x = st.slider("Select a number", 0, 100)
+map_data = pd.DataFrame(np.random.randn(200, 2) / [10, 12] + [23.2, 120.3], columns=["lat", "lon"])
+map_data1 = pd.DataFrame(np.random.randn(100, 2) / [10, 10] + [25, 121.5], columns=["lat", "lon"])
+map_data_all = pd.concat([map_data, map_data1], ignore_index=True)
+
+st.map(map_data_all)
+
+if st.checkbox("Show dataframe"):
+    chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+
+    st.line_chart(chart_data)
+df = pd.DataFrame(
+    np.array([[i, j] for i, j in zip(["拍打頭部", "餵食", "抱抱"], ["蘋果", "芭樂", "橘子"])]), columns=["first column", "second q"]
+)
+option = st.sidebar.selectbox("小兔生氣應該怎麼處理膩?", df["first column"])
+option1 = st.sidebar.selectbox("小兔打世紀帝國喜歡吃什麼水果?", df["second q"])
+
+"You selected: ", option
+"You selected: ", option1
+
+x = st.sidebar.slider("抱多久好呢", 0, 100)
 st.write("You selected", x)
+
+left_column, right_column = st.beta_columns(2)
+pressed = left_column.button('喜歡請給讚?')
+if pressed:
+    right_column.write("我也是100萬喜歡歐!")
+
+expander = st.beta_expander("長輩圖功能")
+expander.write("早安，凡事順心就好")
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.1)
+
+'...and now we\'re done!'
