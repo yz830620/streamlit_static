3520dae9aa8ee85ff6673b4187f3570f89d34dc0
yz830620 <yz830620@gmail.com>
2020-12-25 00:32:00 +0800

    add sklern file requirements

--- sklearn_diabetes.py
+++ sklearn_diabetes.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
