56b0ce78bd5d264ecc0c1da8e788e346d3a818b6
yz830620 <yz830620@gmail.com>
2020-12-24 23:54:11 +0800

    add sklern file

--- test_np.py
+++ test_np.py
@@ -1,21 +1,27 @@
 import streamlit as st
-import time
+import pandas as pd
 import numpy as np
+import time
 
-st.text('This will appear first')
-# Appends some text to the app.
+# Get some data.
+data = np.random.randn(10, 3)
 
-my_slot1 = st.empty()
-# Appends an empty slot to the app. We'll use this later.
-
-my_slot2 = st.empty()
-# Appends another empty slot.
-
-st.text('This will appear last')
-# Appends some more text to the app.
-
-my_slot1.text('This will appear second')
-# Replaces the first empty slot with a text string.
-
-my_slot2.line_chart(np.random.randn(20, 2))
-# Replaces the second empty slot with a chart.
+# Show the data as a chart.
+chart = st.line_chart(data)
+latest_iteration = st.empty()
+bar = st.progress(0)
+for i in range(0,25):
+    # Wait 1 second, so the change is clearer.
+    for j in range(1,5):
+        time.sleep(0.1)
+        latest_iteration.text(f'Iteration {i*4+j}, progess {i*4+j}%')
+        bar.progress(i*4 + j)
+    # Grab some more data.
+    data2 = np.random.randn(10, 1) + 5 * np.log(i+1)
+    data3 = np.random.randn(10, 1) + i**1/4 - 5 * np.log(i+1)
+    data4 = np.random.randn(10, 1) + 12 * np.log(i+1) - i**2/11
+    #st.write(np.log(i))
+    data2 = np.concatenate((data2,data3,data4), axis=1)
+    #st.write(data2)
+    # Append the new data to the existing chart.
+    chart.add_rows(data2)
