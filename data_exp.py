2b5879dd38fe1fffacb4143ddf96e47612e57f14
yz830620 <yz830620@gmail.com>
2020-12-24 16:53:30 +0800

    upload data_exp.py

--- data_exp.py
+++ data_exp.py
@@ -1,5 +1,70 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import pydeck as pdk
 
 st.title('Uber pickups in NYC')
+
+DATE_COLUMN = 'date/time'
+DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
+         'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
+
+@st.cache
+def load_data(nrows):
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Load 10,000 rows of data into the dataframe.
+data = load_data(10000)
+# Notify the reader that the data was successfully loaded.
+data_load_state.text("Done! (using st.cache)")
+
+st.subheader('Raw data')
+st.dataframe(data)
+
+st.subheader('Number of pickups by hour')
+
+hist_values = np.histogram(
+    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
+h = [17]
+hour_to_filter = st.slider('slide the hour bar to change the fig below', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+#filtered_data = data
+#st.subheader(f'Map of all pickups at {h}o\'clock')
+#st.map(filtered_data)
+
+
+st.pydeck_chart(pdk.Deck(
+    map_style='mapbox://styles/mapbox/light-v9',
+    initial_view_state=pdk.ViewState(
+        latitude= 40.75,
+        longitude= -74,
+        zoom=11,
+        pitch=60,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=filtered_data,
+           get_position='[lon, lat]',
+           radius=200,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=filtered_data,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ], 
+))
