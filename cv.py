008a1ef6a6a2ce5c635cd39ef5e23977aad47941
yz830620 <yz830620@gmail.com>
2021-01-15 00:21:57 +0800

    add for heroku

--- cv.py
+++ cv.py
@@ -7,7 +7,7 @@ from PIL import Image
 
 
 def face_reg(img):
-    # load face cascade and eye cascade
+    # load face cascade
     face_cascade = cv2.CascadeClassifier(
         utils.get_haarcascade_path('haarcascade_frontalface_default.xml'))
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -22,7 +22,7 @@ def face_reg(img):
 
 st.title("Detect faces")
 st.markdown('Please use .jpg file')
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg",'jpeg'])
 
 if uploaded_file is not None:
     image = np.array(Image.open(uploaded_file))
